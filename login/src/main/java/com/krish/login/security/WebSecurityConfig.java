655ea38f250a2d8b888344e578863fe015c47cee
krishpranav <krisna.pranav@gmail.com>
2023-06-25 15:51:30 +0530

    login: security[WebSecurityConfigController]

--- login/src/main/java/com/krish/login/security/WebSecurityConfig.java
+++ login/src/main/java/com/krish/login/security/WebSecurityConfig.java
@@ -1,5 +1,53 @@
 package com.krish.login.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.krish.login.security.jwt.AuthEntryPointJwt;
+import com.krish.login.security.jwt.AuthTokenFilter;
+import com.krish.login.security.services.UserDetailsServiceImpl;
+
+
+@Configuration
+@EnableMethodSecurity
 public class WebSecurityConfig {
-    
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    private AuthEntryPointJwt unAuthEntryPointJwt;
+
+    @Bean
+    public AuthTokenFilter authenticationJwAuthTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+
+        return http.build();
+    }
 }
