e07cb169557113e0946eabe51f05bcff7d5b7ea9
krishpranav <krisna.pranav@gmail.com>
2023-06-25 15:43:49 +0530

    login: security[services(UserDetailsServiceIMPL)]

---
+++ login/src/main/java/com/krish/login/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.krish.login.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.krish.login.model.User;
+import com.krish.login.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("user not found with the given username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+
+}
